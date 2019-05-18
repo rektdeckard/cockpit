72af7c29f9d17e5f2d420494cf920af9a94aaa3b
Tobias Fried <friedtm@gmail.com>
2019-05-18 19:53:12 -0400

    High-contrast app theme

--- app/src/main/java/com/tobiasfried/phosphor/activities/MainActivity.java
+++ app/src/main/java/com/tobiasfried/phosphor/activities/MainActivity.java
@@ -8,14 +8,15 @@ import com.tobiasfried.phosphor.licenses.License;
 
 public class MainActivity extends CandyBarMainActivity {
 
-    @NonNull
-    @Override
-    public ActivityConfiguration onInit() {
-        return new ActivityConfiguration()
-                .setLicenseCheckerEnabled(License.isLicenseCheckerEnabled())
-                .setLicenseKey(License.getLicenseKey())
-                .setRandomString(License.getRandomString())
-                .setDonationProductsId(License.getDonationProductsId())
-                .setPremiumRequestProducts(License.getPremiumRequestProductsId(), License.getPremiumRequestProductsCount());
-    }
+	@NonNull
+	@Override
+	public ActivityConfiguration onInit() {
+		return new ActivityConfiguration()
+				.setLicenseCheckerEnabled(License.isLicenseCheckerEnabled())
+				.setLicenseKey(License.getLicenseKey())
+				.setRandomString(License.getRandomString())
+				.setDonationProductsId(License.getDonationProductsId())
+				.setPremiumRequestProducts(License.getPremiumRequestProductsId(), License.getPremiumRequestProductsCount());
+	}
+
 }
